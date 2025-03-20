31b2131194af493c054640cfb33c60c0dcae5231
unknown <lavaroma94@gmail.com>
2025-03-20 17:19:15 +0100

    Eight commit

--- todo/settings.py
+++ todo/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-"default": {
-    "ENGINE": "django.db.backends.sqlite3",
-    "NAME": "db.sqlite3",
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "todo_db",
+        "USER": "postgres",
+        "PASSWORD": "Hellog@71994",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
